9b1045994cff33c9177bc79b08493f2d194b5d42
Lenny Nilsson <lenny.nilsson@expressen.se>
2017-02-06 13:02:37 +0100

    Code cleanup

--- app/src/main/kotlin/se/bylenny/skeleton/MainActivity.kt
+++ app/src/main/kotlin/se/bylenny/skeleton/MainActivity.kt
@@ -4,16 +4,15 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
-import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.button
 
-class KotlinActivity : Activity() {
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_main)
 
-        val next = findViewById(R.id.Button02) as Button
-        next.setOnClickListener {
+        button.setOnClickListener {
             val intent: Intent = Intent()
             setResult(RESULT_OK, intent)
             finish()
@@ -22,7 +21,7 @@ class KotlinActivity : Activity() {
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main_activity2, menu)
+        menuInflater.inflate(R.menu.main, menu)
         return true
     }
 }
